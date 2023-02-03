044082e154f316a372e9679aa44f2e10c5c9b477
Oleksandr Kuvshynov <oleksandr@oleksandrs-air.mynetworksettings.com>
2023-02-03 15:21:07 -0500

    mnk: better benchmark for ANE

--- scripts/benchmark_coreml_model.py
+++ scripts/benchmark_coreml_model.py
@@ -1,32 +1,48 @@
 import coremltools as ct
 import numpy as np
 import time
+import torch
+from fake_model import ActionValueModel
 
-cpu, gpu, ne = True, True, True
+cpu, gpu, ne = False, False, True
 
-for log_batch_size in range(4):
+def to_coreml(torch_model, batch_size, compute_units):
+    torch_model = torch_model.cpu()
+    torch_model.eval()
+    sample = torch.rand(batch_size, 2, 8, 8)
+
+    traced_model = torch.jit.trace(torch_model, sample)
+    return ct.convert(
+        traced_model,
+        inputs=[ct.TensorType(shape=sample.shape)],
+        compute_units=compute_units
+    )
+
+model = ActionValueModel()
+
+for log_batch_size in range(10):
     batch_size = 2 ** log_batch_size
     sample = {'x': np.random.rand(batch_size, 2, 8, 8)}
     if cpu:
-        cpu_model = ct.models.MLModel(f'../_out/8x8/coreml_model_cp_{batch_size}.mlmodel', compute_units=ct.ComputeUnit.CPU_ONLY)
+        cpu_model = to_coreml(model, batch_size, compute_units=ct.ComputeUnit.CPU_ONLY)
         start = time.time()
-        for rep in range(10000):
+        for rep in range(1000):
             out = cpu_model.predict(sample)
 
-        print(f'cpu: 10k x {batch_size} {time.time() - start}')
+        print(f'cpu: 1k x {batch_size} {time.time() - start}')
 
     if gpu:
-        gpu_model = ct.models.MLModel(f'../_out/8x8/coreml_model_cp_{batch_size}.mlmodel', compute_units=ct.ComputeUnit.CPU_AND_GPU)
+        gpu_model = to_coreml(model, batch_size, compute_units=ct.ComputeUnit.CPU_AND_GPU)
         start = time.time()
-        for rep in range(10000):
+        for rep in range(1000):
             out = gpu_model.predict(sample)
 
-        print(f'gpu: 10k x {batch_size} {time.time() - start}')
+        print(f'gpu: 1k x {batch_size} {time.time() - start}')
     
     if ne:
-        ne_model = ct.models.MLModel(f'../_out/8x8/coreml_model_cp_{batch_size}.mlmodel', compute_units=ct.ComputeUnit.CPU_AND_NE)
+        ne_model = to_coreml(model, batch_size, compute_units=ct.ComputeUnit.CPU_AND_NE)
         start = time.time()
-        for rep in range(10000):
+        for rep in range(1000):
             out = ne_model.predict(sample)
 
-        print(f'ne: 10k x {batch_size} {time.time() - start}')
+        print(f'ne: 1k x {batch_size} {time.time() - start}')
---
+++ scripts/fake_model.py
@@ -0,0 +1,53 @@
+# for now just action but with residual connections
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), # TODO: bias=False ?
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1), # TODO: bias=False ?
+            nn.BatchNorm2d(out_channels)
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        out = self.conv(x)
+        out += residual
+        return self.relu(out)
+
+channels = 256
+m, n = 8, 8
+
+class ActionValueModel(nn.Module):
+    def __init__(self):
+        super(ActionValueModel, self).__init__()
+        self.action = nn.Sequential(
+            nn.Conv2d(2, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            #ResidualBlock(channels, channels),
+            ResidualBlock(channels, channels),
+            ResidualBlock(channels, channels),
+            nn.Conv2d(channels, 2, kernel_size=1),
+            nn.BatchNorm2d(2),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(2 * m * n, m * n),
+            nn.ReLU(),
+            nn.Linear(m * n, m * n),
+            nn.LogSoftmax(dim=1),
+        )
+    
+    def forward(self, x):
+        return self.action(x)
